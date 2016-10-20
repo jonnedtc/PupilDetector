1ec2eca81afcb0bfbd3ce1866556f695111c6572
Jonne Engelberts <jonneengelberts@gmail.com>
2016-10-20 14:24:35 +0200

    Added locatePartial function

--- PupilDetector.py
+++ PupilDetector.py
@@ -101,7 +101,106 @@ class GradientIntersect:
 					scores[cy,cx] = score * (1-blurred[cy,cx])
 
 		# get maximum value index 
-		(yval,xval) = np.where(scores==np.max(scores))
+		(yval,xval) = np.unravel_index(np.argmax(scores),scores.shape)
 		
 		# return values
 		return (yval, xval)
+
+	def locatePartial(self, image, (py, px), radius):
+		
+		# make sure py, px is within image radius
+		px = min(max(px,0),self.width)
+		py = min(max(py,0),self.height)
+
+		# radius can only be half of the image at max
+		if radius*2 + 1 > self.width:
+			radius = int(self.width / 2) - 1
+			print("PupilDetector: resized radius to: " + str(radius))
+		if radius*2 + 1 > self.height:
+			radius = int(self.height / 2) - 1
+			print("PupilDetector: resized radius to: " + str(radius))
+
+		# make sure the new box is within image radius
+		if px - radius < 0:
+			px = radius
+		if px + radius > self.width:
+			px = self.width - radius
+		if py - radius < 0:
+			py = radius
+		if py + radius > self.height:
+			py = self.height - radius
+
+		# get partial image
+		image = image[py-radius:py+radius+1,px-radius:px+radius+1]
+
+		# get partial grid
+		ydif = self.height - radius*2 - 1
+		xdif = self.width - radius*2 - 1
+		partialGrid = self.grid[0+ydif:self.height*2-1-ydif, 0+xdif:self.width*2-1-ydif]
+
+		# get image size
+		Y, X = image.shape
+		
+		# create empty score matrix
+		scores = np.zeros((Y,X))
+
+		# normalize image
+		image = (image.astype('float') - np.min(image)) / np.max(image)
+
+		# blur image
+		blurred = gaussian_filter(image, sigma=5)
+		retain = np.percentile(blurred, self.area)
+
+		# calculate gradients
+		gy, gx = np.gradient(image)
+		gx = np.reshape(gx, (X*Y,1))
+		gy = np.reshape(gy, (X*Y,1))
+		gradient = np.hstack((gy,gx))
+
+		# normalize gradients
+		gradient2 = np.sqrt(np.einsum('ij,ij->i',gradient,gradient))
+		gradient2[gradient2==0] = 1
+		gradient /= gradient2[:,np.newaxis]
+
+		# loop through all pixels
+		for cy in range(0,Y,self.accuracy):
+			for cx in range(0,X,self.accuracy):
+
+				if image[cy,cx] < retain:
+
+					# select displacement
+					displacement = partialGrid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
+					displacement = np.reshape(displacement,(X*Y,2))
+
+					# calculate score
+					score = np.einsum('ij,ij->i', displacement, gradient)
+					score = np.einsum('i,i->', score, score)
+					scores[cy,cx] = score
+
+		# multiply with the blurred darkness
+		scores = scores * (1-blurred)
+
+		# if we skipped pixels, get more accurate around the best pixel
+		if self.accuracy>1:
+
+			# get maximum value index 
+			(yval,xval) = np.where(scores==np.max(scores))
+
+			# loop through new pixels
+			for cy in range(yval-self.accuracy,yval+self.accuracy+1):
+				for cx in range(xval-self.accuracy,xval+self.accuracy+1):
+
+					# select displacement
+					displacement = partialGrid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
+					displacement = np.reshape(displacement,(X*Y,2))
+
+					# calculate score
+					score = np.einsum('ij,ij->i', displacement, gradient)
+					score = np.einsum('i,i->', score, score)
+					scores[cy,cx] = score * (1-blurred[cy,cx])
+
+		# get maximum value index 
+		(yval,xval) = np.unravel_index(np.argmax(scores),scores.shape)
+		
+		# return values
+		return (py+yval-radius, px+xval-radius)
