522cd3325a3c80e8d9b54826370f1b429165e7ce
Jonne Engelberts <jonneengelberts@gmail.com>
2016-11-07 19:33:26 +0100

    Replaced functions with locate and track, no more need for init parameters.

--- PupilDetector.py
+++ PupilDetector.py
@@ -13,16 +13,6 @@ from scipy.ndimage.filters import gaussian_filter
 
 class GradientIntersect:
 
-	def __init__(self, (height, width), threshold=100, step=1):
-
-		self.height = height
-		self.width = width
-		self.grid = self.createGrid(height, width)
-		self.area = threshold
-		self.accuracy = step
-		if self.accuracy < 1:
-			self.accuracy = 1
-
 	def createGrid(self, Y, X):
     
 		# create grid vectors
@@ -36,23 +26,12 @@ class GradientIntersect:
 		# reshape grid for easier displacement selection
 		grid = grid.reshape(Y*2-1,X*2-1,2)
 		
-		# return values
 		return grid
 
-	def locate(self, image):
-		
+	def createGradient(self, image):
+
 		# get image size
 		Y, X = image.shape
-		
-		# create empty score matrix
-		scores = np.zeros((Y,X))
-
-		# normalize image
-		image = (image.astype('float') - np.min(image)) / np.max(image)
-
-		# blur image
-		blurred = gaussian_filter(image, sigma=5)
-		retain = np.percentile(blurred, self.area)
 
 		# calculate gradients
 		gy, gx = np.gradient(image)
@@ -65,40 +44,60 @@ class GradientIntersect:
 		gradient2[gradient2==0] = 1
 		gradient /= gradient2[:,np.newaxis]
 
+		return gradient
+
+	def locate(self, image, accuracy = 1):
+		
+		# get image size
+		Y, X = image.shape
+
+		# get grid
+		grid = self.createGrid(Y, X)
+		
+		# create empty score matrix
+		scores = np.zeros((Y,X))
+
+		# normalize image
+		image = (image.astype('float') - np.min(image)) / np.max(image)
+
+		# blur image
+		blurred = gaussian_filter(image, sigma=5)
+
+		# get gradient 
+		gradient = self.createGradient(image)
+
 		# loop through all pixels
-		for cy in range(0,Y,self.accuracy):
-			for cx in range(0,X,self.accuracy):
+		for cy in range(0,Y,accuracy):
+			for cx in range(0,X,accuracy):
 
-				if image[cy,cx] < retain:
+				# select displacement
+				displacement = grid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
+				displacement = np.reshape(displacement,(X*Y,2))
 
-					# select displacement
-					displacement = self.grid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
-					displacement = np.reshape(displacement,(X*Y,2))
-
-					# calculate score
-					score = np.einsum('ij,ij->i', displacement, gradient)
-					score = np.einsum('i,i->', score, score)
-					scores[cy,cx] = score
+				# calculate score
+				score = np.einsum('ij,ij->i', displacement, gradient)
+				score = np.einsum('i,i->', score, score)
+				scores[cy,cx] = score
 
 		# multiply with the blurred darkness
 		scores = scores * (1-blurred)
 
 		# if we skipped pixels, get more accurate around the best pixel
-		if self.accuracy>1:
+		if accuracy>1:
 
 			# get maximum value index 
 			(yval,xval) = np.unravel_index(np.argmax(scores),scores.shape)
 
 			# prevent maximum value index from being close to 0 or max
-			yval = min(max(yval,self.accuracy), Y-self.accuracy-1)
-			xval = min(max(xval,self.accuracy), X-self.accuracy-1)
+			yval = min(max(yval,accuracy), Y-accuracy-1)
+			xval = min(max(xval,accuracy), X-accuracy-1)
 
 			# loop through new pixels
-			for cy in range(yval-self.accuracy,yval+self.accuracy+1):
-				for cx in range(xval-self.accuracy,xval+self.accuracy+1):
+			for cy in range(yval-accuracy,yval+accuracy+1):
+				for cx in range(xval-accuracy,xval+accuracy+1):
 
 					# select displacement
-					displacement = self.grid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
+					displacement = grid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
 					displacement = np.reshape(displacement,(X*Y,2))
 
 					# calculate score
@@ -112,40 +111,16 @@ class GradientIntersect:
 		# return values
 		return (yval, xval)
 
-	def locatePartial(self, image, (py, px), radius):
+	def track(self, image, (py, px), radius=50, distance = 10):
 		
-		# make sure py, px is within image radius
-		px = min(max(px,0),self.width)
-		py = min(max(py,0),self.height)
-
-		# radius can only be half of the image at max
-		if radius*2 + 1 > self.width:
-			radius = int(self.width / 2) - 1
-			print("PupilDetector: resized radius to: " + str(radius))
-		if radius*2 + 1 > self.height:
-			radius = int(self.height / 2) - 1
-			print("PupilDetector: resized radius to: " + str(radius))
-
-		# make sure the new box is within image radius
-		if px - radius < 0:
-			px = radius
-		if px + radius > self.width:
-			px = self.width - radius
-		if py - radius < 0:
-			py = radius
-		if py + radius > self.height:
-			py = self.height - radius
-
-		# get partial image
-		image = image[py-radius:py+radius+1,px-radius:px+radius+1]
-
-		# get partial grid
-		ydif = self.height - radius*2 - 1
-		xdif = self.width - radius*2 - 1
-		partialGrid = self.grid[0+ydif:self.height*2-1-ydif, 0+xdif:self.width*2-1-ydif]
+		# select image
+		image = image[py-radius:py+radius+1, px-radius:px+radius+1]
 
 		# get image size
 		Y, X = image.shape
+
+		# get grid
+		grid = self.createGrid(Y, X)
 		
 		# create empty score matrix
 		scores = np.zeros((Y,X))
@@ -155,65 +130,32 @@ class GradientIntersect:
 
 		# blur image
 		blurred = gaussian_filter(image, sigma=5)
-		retain = np.percentile(blurred, self.area)
 
-		# calculate gradients
-		gy, gx = np.gradient(image)
-		gx = np.reshape(gx, (X*Y,1))
-		gy = np.reshape(gy, (X*Y,1))
-		gradient = np.hstack((gy,gx))
-
-		# normalize gradients
-		gradient2 = np.sqrt(np.einsum('ij,ij->i',gradient,gradient))
-		gradient2[gradient2==0] = 1
-		gradient /= gradient2[:,np.newaxis]
+		# get gradient 
+		gradient = self.createGradient(image)
 
 		# loop through all pixels
-		for cy in range(0,Y,self.accuracy):
-			for cx in range(0,X,self.accuracy):
+		for cy in range(radius-distance, radius+distance+1):
+			for cx in range(radius-distance, radius+distance+1):
+				
+				# select displacement
+				displacement = grid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
+				displacement = np.reshape(displacement,(X*Y,2))
 
-				if image[cy,cx] < retain:
-
-					# select displacement
-					displacement = partialGrid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
-					displacement = np.reshape(displacement,(X*Y,2))
-
-					# calculate score
-					score = np.einsum('ij,ij->i', displacement, gradient)
-					score = np.einsum('i,i->', score, score)
-					scores[cy,cx] = score
+				# calculate score
+				score = np.einsum('ij,ij->i', displacement, gradient)
+				score = np.einsum('i,i->', score, score)
+				scores[cy,cx] = score
 
 		# multiply with the blurred darkness
 		scores = scores * (1-blurred)
-
-		# if we skipped pixels, get more accurate around the best pixel
-		if self.accuracy>1:
-
-			# get maximum value index 
-			(yval,xval) = np.unravel_index(np.argmax(scores),scores.shape)
-
-			# prevent maximum value index from being close to 0 or max
-			yval = min(max(yval,self.accuracy), Y-self.accuracy-1)
-			xval = min(max(xval,self.accuracy), X-self.accuracy-1)
-
-			# loop through new pixels
-			for cy in range(yval-self.accuracy,yval+self.accuracy+1):
-				for cx in range(xval-self.accuracy,xval+self.accuracy+1):
-
-					# select displacement
-					displacement = partialGrid[Y-cy-1:Y*2-cy-1,X-cx-1:X*2-cx-1,:]
-					displacement = np.reshape(displacement,(X*Y,2))
-
-					# calculate score
-					score = np.einsum('ij,ij->i', displacement, gradient)
-					score = np.einsum('i,i->', score, score)
-					scores[cy,cx] = score * (1-blurred[cy,cx])
 
 		# get maximum value index 
 		(yval,xval) = np.unravel_index(np.argmax(scores),scores.shape)
 		
 		# return values
 		return (py+yval-radius, px+xval-radius)
+	
 
 class IsophoteCurvature:
 
